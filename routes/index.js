c89027eec487000df3b2a453a933d4ee8b216df0
Aula <Aula@EQUIP079578.xunta.local>
2020-11-11 11:05:14 +0100

    esto no tira ni pa dios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+const Mensaje = require('../models/mensaje');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  let mensajes = await Mensaje.findAll();
+  if (mensajes) {
+    res.render('index', { title: 'Express', mensajes });
+  } else {
+    res.render('index',  { title: 'Express', mensajes: [] })
+  }
+});
+
+
+router.post('/publicar', async function (req, res) {
+  await Mensaje.create({...req.body, fechaHora: new Date()});
+  res.redirect('/');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,41 @@
 var express = require('express');
+var Usuario = require('../models/usuario');
+
+
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  let usuarios = await Usuario.findAll();
+  res.render("listado-usuarios", {usuarios});
 });
+
+router.get('/nuevo', function (req, res) {
+  res.render("alta-usuario");
+})
+
+router.post('/nuevo', async function (req, res) {
+    // Obtención de los datos del formulario
+    let {nombre, apellidos, email, password, repassword, ubicacion, rol} = req.body;
+
+    if (password == repassword) {
+      let usuario = new Usuario({nombre, apellidos, email, password, ubicacion, rol});
+      await usuario.save();
+      res.redirect("/usuarios");
+    } else {
+      //TODO: mostrar error
+    }
+})
+
+
+router.get('/:id', function (req, res) {
+  res.render("detalle-usuario");  
+})
+
+router.post('/:id', function (req, res) {
+  
+})
+
+
 
 module.exports = router;
