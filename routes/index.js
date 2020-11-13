3cd60e98371244087bd29503fce0fcf2c76fb101
Aula <Aula@EQUIP079578.xunta.local>
2020-11-13 11:58:06 +0100

    LOGIN + /USERS/ID

--- routes/index.js
+++ routes/index.js
@@ -19,11 +19,31 @@ router.post('/',  async function (req, res) {
     });
     if (usuario) {
         req.session.usuario = usuario;
-        res.redirect("/usuario");
+        res.redirect("/users/" + usuario.id);
     } else {
         res.render("login", {error: "Email o contraseña incorrectos"});
     }
 })
+
+
+router.get('/registro', function (req, res) {
+  res.render("registro");
+})
+
+router.post('/registro', async function (req, res) {
+    // Obtención de los datos del formulario
+    let {nombre, apellidos, ubicacion, email, password, repassword} = req.body;
+
+      if (password == repassword) {
+      let usuario = new Usuario({nombre, apellidos, ubicacion, email, password, repassword});
+      await usuario.save();
+      res.redirect("/");
+    } else {
+      res.render("registro", {error: "Datos no válidos"});
+       }
+})
+
+
 
 router.get('/passwordlost', function (req, res, next) {
     res.render("passwordlost");
--- routes/users.js
+++ routes/users.js
@@ -10,28 +10,18 @@ router.get('/', async function(req, res, next) {
   res.render("userlist", {usuarios});
 });
 
-router.get('/registro', function (req, res) {
-  res.render("registro");
+
+router.get('/:id', async function (req, res) {
+  let id = req.params.id;
+  try { 
+    let usuario = await Usuario.findByPk(id);
+    res.render("pedido-lista", {usuario});  
+  } catch(err) {
+    res.render("error");
+  }
 })
 
-router.post('/registro', async function (req, res) {
-    // Obtención de los datos del formulario
-    let {nombre, apellidos, ubicacion, email, password, repassword} = req.body;
-
-    if (password == repassword) {
-      let usuario = new Usuario({nombre, apellidos, ubicacion, email, password, repassword});
-      await usuario.save();
-      res.redirect("registro");
-    } else {
-       }
-})
-
-
-router.get('/usuario/:id', function (req, res) {
-  res.render("detalle-usuario");  
-})
-
-router.post('/usuario/:id', function (req, res) {
+router.post('/id', function (req, res) {
   
 })
 
