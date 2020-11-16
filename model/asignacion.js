f4f8ea4f254cfa31cffdc83a5b8c8d61c2134423
Aula <Aula@EQUIP079580.xunta.local>
2020-11-16 09:41:07 +0100

    modelo asignacion

--- model/asignacion.js
+++ model/asignacion.js
@@ -1,1 +1,18 @@
 //añadir campo cantidad
+const { Sequelize, DataTypes, Model } = require('sequelize');
+
+class Asignacion extends Model {
+  // definiciones de atributos y métodos
+
+  static init(sequelize) {
+    super.init({
+        cantidad: {type: DataTypes.INTEGER}
+      }, {
+        sequelize,
+        modelName: 'Asignacion'
+      });
+      
+    }
+}
+
+module.exports = Asignacion;
