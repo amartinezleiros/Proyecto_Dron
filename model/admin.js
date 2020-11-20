b40531d8f977742d481c78f49d27da2c9b33fde2
Aula <Aula@EQUIP079579.xunta.local>
2020-11-20 11:36:02 +0100

    cambio jsdoc

--- model/admin.js
+++ model/admin.js
@@ -1,9 +1,14 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
 const Usuario = require('./Usuario')
-
+/**
+ * algo a ver si aparece 
+ */
 class Admin extends Usuario {
   // definiciones de atributos y métodos
-
+  /**
+   * ñlkasdñljasñlkfdjñlakdfjñalkjfdñkv
+   * @param {*} sequelize 
+   */
   static init(sequelize) {
     super.init({
 
